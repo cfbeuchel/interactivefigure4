4cea0e08557ecf49ccd73f2e4d902f2228122b8a
cfbeuchel <cfbeuchel@imise.uni-leipzig.de>
2019-09-09 10:16:18 +0200

    add CI-CD config and necessary changes to setup

--- functions/app_dependencies.R
+++ functions/app_dependencies.R
@@ -1,15 +1,16 @@
 # which packages do I need for this app
-necessary.packages <- c(
-  "shiny", 
-  "magrittr",
-  "data.table",
-  "visNetwork")
+# necessary.packages <- c(
+#   "shiny", 
+#   "magrittr",
+#   "data.table",
+#   "visNetwork")
 
 # check for installed packages and install them if necessary
-installed <- installed.packages()
-needed <- necessary.packages
-to.install <- needed[!(needed %in% installed[,1])]
-rm(installed)
+# installed <- installed.packages()
+# needed <- necessary.packages
+# to.install <- needed[!(needed %in% installed[,1])]
+# rm(installed)
+
 # if(length(to.install)!=0){
 #   if("sva" %in% to.install){
 #     install.packages("BiocManager")
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+R -q -e "install.packages(c('visNetwork'), repos = 'https://cran.rstudio.com', quiet = TRUE)"
+R -q -e "install.packages(c('shiny'), quiet = TRUE)"
+R -q -e "install.packages(c('magrittr'), quiet = TRUE)"
+R -q -e "install.packages(c('data.table'), quiet = TRUE)"
+
